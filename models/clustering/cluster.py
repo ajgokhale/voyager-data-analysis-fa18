0feaadfc0471e53a662bdfce2af5364fe8c420c6
Jake <jweppner@berkeley.edu>
2018-11-13 15:41:48 -0800

    changed False to True

--- models/clustering/cluster.py
+++ models/clustering/cluster.py
@@ -11,15 +11,14 @@ def clusterKMeans(df):
     # and returns a new dataframe with an additional column specifying the customer's
     # cluster (from 0 to n)
 
-    # The cluster should also be validated using internal validation techniques (see validateCluster)
-
+    
     return None
 
 def validateCluster(something):
     # Function that validates the given clustering, returns True if the cluster should be
     # considered
 
-    return False
+    return True
 
 def plotCluster(df):
     # Takes in a 2D or 3D dataframe of customer metrics and associated clusters,
