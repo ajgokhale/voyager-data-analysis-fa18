5bbc6ca3dd1982d573fbc7eec2d03ad632c322ef
Rahul Hayaran <rahul.hayaran@gmail.com>
2018-12-04 14:33:09 -0800

    added support for validation

--- write_data.py
+++ write_data.py
@@ -12,3 +12,9 @@ def save_plots(plots, name):
 		figi      = plot[0]
 		plot_name = plot[1]
 		figi.savefig("output/scatters/"+name+"/"+plot_name+".png", format='png')
+
+def save_validation(validation_list):
+	o = open("validation/validation_vals.out", 'w+')
+	for validation in validation_list:
+		o.write("X Indices: " + str(validation[2]) + "\n" + "Y Index: " + str(validation[3]) + "\n"+ 'K-folds Score: ' + str(validation[0] * 100) + '% ' + '\n' + 'AIC Score: ' + str(validation[1]) + '\n\n')
+	o.close()
